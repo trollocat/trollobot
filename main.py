078f55f820aec3f0339a5d5249c8085adc07330c
Joan Ignacio Gonzalez <trollocat@gmail.com>
2024-09-17 19:37:26 -0300

    Refactored utils, added neg offset for [kk] patterns, added hit.png, made GIFs taller for better readibility on phones

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import discord
 import logging
 from utils import *
 from dotenv import load_dotenv
+from discord import app_commands
 from discord.ext import commands
 
 # main = False | dev = True
@@ -22,7 +23,7 @@ else:
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = commands.Bot(command_prefix='/', intents=intents)
+client = commands.Bot(command_prefix='!', intents=intents)
 
 logging.basicConfig(level=logging.INFO)
 
@@ -32,60 +33,102 @@ async def on_ready():
     print(f'Loggeado como {client.user}')
 
 
-@client.tree.command(name="sync", description="Sincroniza slash commands")
-async def sync(interaction: discord.Interaction):
-    if interaction.user.id == int(trollocat_id):
-        await client.tree.sync()
-        await interaction.response.send_message("Slash commands sincronizados")
-    else:
-        await interaction.response.send_message("Solo el todopoderoso trollocat puede usar este comando")
+@client.hybrid_command()
+async def sync(ctx: commands.Context):
+    await ctx.send("Slash commands sincronizados")
+    await client.tree.sync()
 
 
 @client.tree.command(name="pinga", description="Genera una imagen a partir de un patrón")
-async def pinga(interaction: discord.Interaction, texto: str):
+@app_commands.describe(texto="Patrón en texto", gif="¿Visualizar animado en GIF?")
+async def pinga(interaction: discord.Interaction, texto: str, gif: bool = False):
     try:
+        await interaction.response.defer()  # Defer the response to avoid timeout
+
         patterns = get_patterns_from_text(texto)
 
-        if "pepiga" in patterns:
-            raise ValueError("patrón mal formulado. Ejemplo correcto: (kkkdddk)kdd(kkkd)d[kkd]d k")
+        if any("pepiga" in pattern[0] for pattern in patterns):
+            raise ValueError("Patrón mal formulado. Ejemplo correcto: (kkkdddk)kdd(kkkd)d[kkd]d k")
 
-        image_paths = [f"{patterns_folder_path}/{pattern}.png" for pattern in patterns]
+        image_paths = [f"{patterns_folder_path}/{pattern[0]}.png" for pattern in patterns]
 
         chunk_size = 16
         image_limit = 8
 
-        if len(image_paths) > chunk_size * image_limit:
-            raise ValueError(
-                f"El resultado daría {math.ceil(len(image_paths) / chunk_size)} imágenes, lo cual es una banda")
+        # GIF
+        if gif:
+            images = [Image.open(path) for path in image_paths]
 
-        for i in range(0, len(image_paths), chunk_size):
-            chunk = image_paths[i:i + chunk_size]
-            montage = create_beatmap_image(chunk)
+            frame_duration_ms = 22
+            # total_width = 2016
+            # max_height = 124
+            total_width = 1512
+            max_height = 124
 
-            with io.BytesIO() as image_binary:
-                montage.save(image_binary, 'PNG')
-                image_binary.seek(0)
+            total_scroll_width = total_width * 2 + sum(img.size[0] for img in images)
 
-                # First interaction
-                if i == 0:
-                    await interaction.response.send_message(
-                        file=discord.File(fp=image_binary,
-                                          filename=f'trollobot_taiko_pattern{i // chunk_size + 1}.png'))
-                else:
-                    await interaction.channel.send(
-                        file=discord.File(fp=image_binary,
-                                          filename=f'trollobot_taiko_pattern{i // chunk_size + 1}.png'))
+            frames = []
+            for offset in range(0, total_scroll_width - total_width + 1, 22):
+                frame = Image.new("RGBA", (total_width, max_height), (255, 255, 255, 0))
+                x_offset = total_width - offset
 
+                for index, img in enumerate(images):
+                    if x_offset + img.size[0] > 0 and x_offset < total_width:
+                        frame.paste(img, (x_offset, 0), img)
+                    x_offset += img.size[0]
+                    if patterns[index][1]:
+                        x_offset -= 62
+
+                frames.append(frame)
+
+                if len(frames) > 1200:
+                    raise ValueError(
+                        f"El gif duraría más de {int(round(len(frames) * frame_duration_ms / 1000, 0))} segundos, lo cual es una banda")
+
+            if frames:
+                with io.BytesIO() as image_binary:
+                    frames[0].save(image_binary, format='GIF', save_all=True, disposal=2, append_images=frames[1:],
+                                   duration=frame_duration_ms, loop=0)
+                    image_binary.seek(0)
+
+                    await interaction.followup.send(
+                        file=discord.File(fp=image_binary, filename='trollobot_taiko_patterns.gif'))
+            else:
+                await interaction.followup.send("No hay frames para crear el GIF", ephemeral=True)
+
+        # PNG
+        else:
+            if len(image_paths) > chunk_size * image_limit:
+                raise ValueError(
+                    f"El resultado daría {math.ceil(len(image_paths) / chunk_size)} imágenes, lo cual es una banda")
+
+            for i in range(0, len(image_paths), chunk_size):
+                chunk = image_paths[i:i + chunk_size]
+                montage = create_beatmap_image(chunk)
+
+                with io.BytesIO() as image_binary:
+                    montage.save(image_binary, 'PNG')
+                    image_binary.seek(0)
+
+                    # First interaction
+                    if i == 0:
+                        await interaction.followup.send(
+                            file=discord.File(fp=image_binary,
+                                              filename=f'trollobot_taiko_pattern{i // chunk_size + 1}.png'))
+                    else:
+                        await interaction.channel.send(
+                            file=discord.File(fp=image_binary,
+                                              filename=f'trollobot_taiko_pattern{i // chunk_size + 1}.png'))
 
 
     except ValueError as ve:
 
-        await interaction.response.send_message(f"Error, {ve}", ephemeral=True)
+        await interaction.followup.send(f"Error. {ve}", ephemeral=True)
 
 
     except Exception as e:
 
-        await interaction.response.send_message(f"Error inesperado, {e}", ephemeral=True)
+        await interaction.followup.send(f"Error inesperado. {e}", ephemeral=True)
 
 
 @client.tree.command(name="tt", description="Genera un mensaje con emojis a partir de un patrón")
@@ -95,7 +138,7 @@ async def tt(interaction: discord.Interaction, texto: str):
         patterns = get_patterns_from_text(texto)
         for emote in patterns:
             if emote == "pepiga":
-                raise ValueError("patrón mal formulado. Ejemplo correcto: (kkkdddk)kdd(kkkd)d[kkd]d k")
+                raise ValueError("Patrón mal formulado. Ejemplo correcto: (kkkdddk)kdd(kkkd)d[kkd]d k")
             emoji = discord.utils.get(client.emojis, name=emote)
             if emoji:
                 mensaje += str(emoji)
@@ -104,15 +147,15 @@ async def tt(interaction: discord.Interaction, texto: str):
         mensaje += "​"  # Invisible zero width character
 
         if len(mensaje) > 1500:
-            raise ValueError("mensaje muy largo")
+            raise ValueError("Mensaje muy largo")
 
         await interaction.response.send_message(mensaje)
 
     except ValueError as ve:
-        await interaction.response.send_message(f"Error, {ve}", ephemeral=True)
+        await interaction.response.send_message(f"Error. {ve}", ephemeral=True)
 
     except Exception as e:
-        await interaction.response.send_message(f"Error inesperado, {e}", ephemeral=True)
+        await interaction.response.send_message(f"Error inesperado. {e}", ephemeral=True)
 
 
 client.run(token)
--- utils.py
+++ utils.py
@@ -1,20 +1,179 @@
 from PIL import Image
 
 
+def do_open_and_closing_symbols_match(input_string, opening_symbol, closing_symbol):
+    stack = []
+    balanced = True
+    index = 0
+
+    while index < len(input_string) and balanced:
+        token = input_string[index]
+
+        if token == opening_symbol:
+            stack.append(token)
+        elif token == closing_symbol:
+            if not stack:
+                balanced = False
+            else:
+                stack.pop()
+
+        index += 1
+
+    return balanced and not stack
+
+
+def text_is_correct(text):
+    return (
+            do_open_and_closing_symbols_match(text, "(", ")")
+            and do_open_and_closing_symbols_match(text, "[", "]")
+            and do_open_and_closing_symbols_match(text, "{", "}")
+    )
+
+
+def get_patterns_from_text(text):
+    normalized_text = text.lower()
+    result = []
+    separators = {"(", ")", "[", "]"}
+    i = 0
+
+    for char in normalized_text:
+        if char not in separators and char not in {"k", "d", " "}:
+            result.append(("pepiga", False))
+            return result
+
+    for count, char in enumerate(normalized_text[:-1]):
+        if char == normalized_text[count + 1] and char in separators:
+            result.append(("pepiga", False))
+            return result
+
+    open_paren_count, close_paren_count = 0, 0
+    open_brack_count, close_brack_count = 0, 0
+
+    for char in normalized_text:
+        if char == "(":
+            open_paren_count += 1
+        elif char == ")":
+            close_paren_count += 1
+        elif char == "[":
+            open_brack_count += 1
+        elif char == "]":
+            close_brack_count += 1
+
+    if (open_paren_count != close_paren_count or open_brack_count != close_brack_count) or (
+            not do_open_and_closing_symbols_match(normalized_text, "(", ")") or
+            not do_open_and_closing_symbols_match(normalized_text, "[", "]")
+    ):
+        result.append(("pepiga", False))
+        return result
+
+    while i < len(normalized_text):
+        if normalized_text[i] not in separators:
+            if normalized_text[i] == " ":
+                result.append(("bk", False))
+            else:
+                result.append(("1" + normalized_text[i], False))
+            i += 1
+
+        elif normalized_text[i] == "(":
+            # Handle 1/6 patterns
+            j = 0
+            # is_one_third = False
+            while normalized_text[i + j + 1] != ")":
+                # if normalized_text[i + j + 1] == " ":
+                #     is_one_third = True
+                j += 1
+
+            # if is_one_third:
+            #     while normalized_text[i + j + 1] != ")" and normalized_text[i + j + 1] != " ":
+            #         j += 1
+
+            if j == 1:
+                result.append((f"1{normalized_text[i + 1]}", True))
+
+            if j == 2:
+                result.append((f"1{normalized_text[i + 1]}6{normalized_text[i + 2]}", False))
+                result.append((f"6{normalized_text[i + 2]}2", False))
+
+            elif j == 3:
+                result.append((f"1{normalized_text[i + 1]}6{normalized_text[i + 2]}", False))
+                result.append((f"6{normalized_text[i + 2]}6{normalized_text[i + 3]}", False))
+                result.append((f"6{normalized_text[i + 3]}1", True))
+
+            elif j == 4:
+                result.append((f"1{normalized_text[i + 1]}6{normalized_text[i + 2]}", False))
+                result.append((f"6{normalized_text[i + 2]}6{normalized_text[i + 3]}", False))
+                result.append((f"6{normalized_text[i + 3]}1{normalized_text[i + 4]}", False))
+
+            elif j % 3 == 1:
+                result.append((f"1{normalized_text[i + 1]}6{normalized_text[i + 2]}", False))
+                result.append((f"6{normalized_text[i + 2]}6{normalized_text[i + 3]}", False))
+                y = 3
+                while y < j - 1:
+                    result.append(
+                        (f"6{normalized_text[i + y]}1{normalized_text[i + y + 1]}6{normalized_text[i + y + 2]}", False))
+                    result.append((f"6{normalized_text[i + y + 2]}6{normalized_text[i + y + 3]}", False))
+                    y += 3
+                result.append((f"6{normalized_text[i + j - 1]}1{normalized_text[i + j]}", False))
+
+            i += j + 2
+
+        elif normalized_text[i] == "[":
+            # Handle 1/8 patterns
+            j = 0
+            while normalized_text[i + j + 1] != "]":
+                j += 1
+
+            if j == 1:
+                result.append((f"1{normalized_text[i + 1]}", True))
+
+            elif j == 2:
+                result.append((f"1{normalized_text[i + 1]}8{normalized_text[i + 2]}", False))
+                result.append((f"8{normalized_text[i + 2]}", True))
+
+            elif j == 3:
+                result.append((f"1{normalized_text[i + 1]}8{normalized_text[i + 2]}", False))
+                result.append((f"8{normalized_text[i + 2]}1{normalized_text[i + 3]}", False))
+
+            elif j == 4:
+                result.append((f"1{normalized_text[i + 1]}8{normalized_text[i + 2]}", False))
+                result.append((f"8{normalized_text[i + 2]}1{normalized_text[i + 3]}8{normalized_text[i + 4]}", False))
+                result.append((f"8{normalized_text[i + 4]}", True))
+
+            elif j >= 5 and j % 2 == 1:
+                y = 1
+                result.append((f"1{normalized_text[i + y]}8{normalized_text[i + y + 1]}", False))
+                while y < j - 2:
+                    result.append(
+                        (f"8{normalized_text[i + y + 1]}1{normalized_text[i + y + 2]}8{normalized_text[i + y + 3]}",
+                         False))
+                    y += 2
+                result.append((f"8{normalized_text[i + y + 1]}1{normalized_text[i + y + 2]}", False))
+
+            elif j >= 6 and j % 2 == 0:
+                y = 1
+                result.append((f"1{normalized_text[i + y]}8{normalized_text[i + y + 1]}", False))
+                while y < j - 1:
+                    result.append(
+                        (f"8{normalized_text[i + y + 1]}1{normalized_text[i + y + 2]}8{normalized_text[i + y + 3]}",
+                         False))
+                    y += 2
+                result.append((f"8{normalized_text[i + y + 1]}", True))
+
+            i += j + 2
+    return result
+
+
 def create_beatmap_image(image_paths):
-    # Load all images
     images = [Image.open(img) for img in image_paths]
 
-    widths, heights = zip(*(i.size for i in images))
+    # widths, heights = zip(*(i.size for i in images)) # useful but not used
 
-    # Total width should be 1984px but discord crop makes 2016px prettier, which is a horizontal margin of 16px
+    # Total width should be 1984px but discord crop makes 2016px prettier, this means a horizontal margin of 16px
     total_width = 2016
     max_height = 124
 
-    # Create a new blank image with the appropriate size in RGBA mode
     map_image = Image.new('RGBA', (total_width, max_height))
 
-    # Paste each image into the new image
     x_offset = 16
     for img in images:
         map_image.paste(img, (x_offset, 0), img)
@@ -23,236 +182,10 @@ def create_beatmap_image(image_paths):
     return map_image
 
 
-def do_open_and_closing_symbols_match(input_string, opening_symbol,
-                                      closing_symbol):
-    s = []
-    balanced = True
-    index = 0
-    while index < len(input_string) and balanced:
-        token = input_string[index]
-        if token == opening_symbol:
-            s.append(token)
-        elif token == closing_symbol:
-            if len(s) == 0:
-                balanced = False
-            else:
-                s.pop()
-
-        index += 1
-
-    return balanced and len(s) == 0
-
-
-def text_is_correct(text):
-    return (do_open_and_closing_symbols_match(text, "(", ")")
-            and do_open_and_closing_symbols_match(
-                text, "[", "]")) and do_open_and_closing_symbols_match(
-        text, "{", "}")
-
-
-def get_patterns_from_text(texto):
-    message = texto
-    acum = []
-    separators = ["(", ")", "[", "]"]
-    message_normalized = ""
-    es_un_tercio = False
-    i = 0
-    j = 0
-
-    # escudo anti boludos
-    for letter in message:
-        message_normalized += letter.lower()
-        if letter.lower() not in {"(", ")", "[", "]", "k", "d", " "}:
-            i = len(message)
-            acum.append("pepiga")
-
-    message = message_normalized
-
-    # detector de duplicado de ()[]
-    for count, letter in enumerate(message):
-        if count < len(message) - 1 and message[count] == message[
-            count + 1] and message[count] in separators:
-            i = len(message)
-            acum.append("pepiga")
-
-    # detector de closings y openings sin match
-    pare_a, pare_c, corc_a, corc_c = 0, 0, 0, 0
-    pare = ""
-    corc = ""
-    for letter in message:
-        if letter == "(":
-            pare_a += 1
-            pare += letter
-        elif letter == ")":
-            pare_c += 1
-            pare += letter
-        elif letter == "[":
-            corc_a += 1
-            corc += letter
-        elif letter == "]":
-            corc_c += 1
-            corc += letter
-    if (pare_a != pare_c or corc_a
-        != corc_c) or (not do_open_and_closing_symbols_match(pare, "(", ")")
-                       or not do_open_and_closing_symbols_match(corc, "[", "]")):
-        i = len(message)
-        acum.append("pepiga")
-
-    while i < len(message):
-        if message[i] not in separators:
-            # modo 1/4
-            if message[i] == " ":
-                acum.append("bk")
-            else:
-                acum.append("1" + message[i])
-            i += 1
-
-        elif message[i] == "(":
-            # modo 1/6
-
-            # obtengo cantidad de x dentro de ()
-            while message[i + j + 1] != ")":
-                if message[i + j + 1] == " ":
-                    es_un_tercio = True
-                j += 1
-
-            # 1/3 experimental
-            if es_un_tercio:
-                while message[i + j + 1] != ")" and message[i + j + 1] != " ":
-                    j += 1
-
-            # un (xx) doble
-            if j == 2:
-                # 1x6x
-                acum.append("1" + message[i + 1] + "6" + message[i + 2])
-
-                # 6x2
-                acum.append("6" + message[i + 2] + "2")
-
-            # un (xxx) raro pato por que
-            if j == 3:
-                # 1x6x
-                acum.append("1" + message[i + 1] + "6" + message[i + 2])
-
-                # 6x6x
-                acum.append("6" + message[i + 2] + "6" + message[i + 3])
-
-                # 6x1
-                acum.append("6" + message[i + 3] + "1")
-
-            # un simple (xxxx)
-            if j == 4:
-                # 1x6x
-                acum.append("1" + message[i + 1] + "6" + message[i + 2])
-
-                # 6x6x
-                acum.append("6" + message[i + 2] + "6" + message[i + 3])
-
-                # 6x1x
-                acum.append("6" + message[i + 3] + "1" + message[i + 4])
-
-            # un (xxx|xxx|x) o más cantidad de xxx|
-            elif j % 3 == 1:
-                # 1x6x
-                acum.append("1" + message[i + 1] + "6" + message[i + 2])
-
-                # 6x6x
-                acum.append("6" + message[i + 2] + "6" + message[i + 3])
-
-                # # # parte loopeada
-                y = 3
-                while y < j - 1:
-                    # # 6x1x6x
-                    acum.append("6" + message[i + y] + "1" + message[i + y + 1] + "6" +
-                                message[i + y + 2])
-
-                    # # 6x6x
-                    acum.append("6" + message[i + y + 2] + "6" + message[i + y + 3])
-
-                    # # pasos de a 3 por (xxx|)
-                    y += 3
-
-                # 6x1x
-                acum.append("6" + message[i + j - 1] + "1" + message[i + j])
-            i += j + 2
-
-        elif message[i] == "[":
-            # modo 1/8
-            print("[")
-
-            # obtengo cantidad de x dentro de []
-            while message[i + j + 1] != "]":
-                j += 1
-
-            # [x] re troll
-            if j == 1:
-                acum.append("1" + message[i + 1])
-
-            # [xx] dobles
-            if j == 2:
-                es_doble = True
-                # 1x8x
-                acum.append("1" + message[i + 1] + "8" + message[i + 2])
-
-                # 8x
-                acum.append("8" + message[i + 2])
-
-            # [xxx]
-            if j == 3:
-                # 1x8x
-                acum.append("1" + message[i + 1] + "8" + message[i + 2])
-
-                # 8x1x
-                acum.append("8" + message[i + 2] + "1" + message[i + 3])
-
-            # [xxxx]
-            # interpretación [abcd] = [1a8b+1b8c1d+8d]
-            elif j == 4:
-                # 1x8x
-                acum.append("1" + message[i + 1] + "8" + message[i + 2])
-
-                # 8x1x8x
-                acum.append("8" + message[i + 2] + "1" + message[i + 3] + "8" +
-                            message[i + 4])
-
-                # 8x
-                acum.append("8" + message[i + 4])
-
-            # [xxxxx]
-            # interpretación [abcde] = [1a8b + 1b8c1d + 1d8e]
-            #                           [1a8b + loopear j/2-2 veces + 1d8e]
-            elif j >= 5 and j % 2 == 1:
-                y = 1
-                # 1x8x
-                acum.append("1" + message[i + y] + "8" + message[i + y + 1])
-
-                # 8x1x8x
-                while y < j - 2:
-                    acum.append("8" + message[i + y + 1] + "1" + message[i + y + 2] +
-                                "8" + message[i + y + 3])
-                    y += 2
-
-                # 8x1x
-                acum.append("8" + message[i + y + 1] + "1" + message[i + y + 2])
-
-            # [xxxxxx]
-            # interpretación [abcdef] = [1a8b + 1b8c1d + 1d8e1f + 8f]
-            #                           [1a8b + loopear j/2-2 veces + 8y+j/2]
-            elif j >= 6 and j % 2 == 0:
-                y = 1
-                # 1x8x
-                acum.append("1" + message[i + y] + "8" + message[i + y + 1])
-
-                # 8x1x8x
-                while y < j - 1:
-                    acum.append("8" + message[i + y + 1] + "1" + message[i + y + 2] +
-                                "8" + message[i + y + 3])
-                    y += 2
-
-                # 8x
-                acum.append("8" + message[i + y + 1])
-
-            i += j + 2
-            print(f"este [] tuvo {j} notas")
-            print("]")
-    return acum
+if __name__ == "__main__":
+    example = "[kkddkkddk]"
+    emoji_message = ""
+    patterns = get_patterns_from_text(example)
+    for emoji in patterns:
+        emoji_message = f"{emoji_message}:{emoji[0]}:"
+    print(emoji_message)
